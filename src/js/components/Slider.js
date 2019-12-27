0cacf293ab4c3b9385f045ceed2680be4ea6c00b
madcido <fabowow@gmail.com>
2019-12-26 21:29:22 -0300

    New slider bullets

--- src/js/components/Slider.js
+++ src/js/components/Slider.js
@@ -3,9 +3,9 @@ import Project from './Project';
 
 export default function Slider({ data, actions }) {
     const [index, setIndex] = useState(0);
-    const [display, setDisplay] = useState(0);
+    const [display, setDisplay] = useState(1);
 
-    function dataToDisplay() {
+    const dataToDisplay = (() => {
           const toDisplay = [];
           let i = index;
           while (toDisplay.length < Math.min(data.length, display)) {
@@ -16,21 +16,19 @@ export default function Slider({ data, actions }) {
                 }
           }
           return toDisplay;
-    }
+    })();
 
     function displayBySize() {
-        if (window.innerWidth < 720) {
+        if (window.innerWidth < 1180) {
           setDisplay(1);
-      } else if (window.innerWidth < 1080) {
-          setDisplay(2);
         } else {
           setDisplay(3);
         }
     };
 
     function slide(direction) {
-        const nextIndex = (data.length + index + direction)%data.length;
-        setIndex(nextIndex);
+        const toIndex = (data.length + index + direction)%data.length;
+        setIndex(toIndex);
     };
 
     useEffect(() => {
@@ -39,12 +37,23 @@ export default function Slider({ data, actions }) {
         return () => window.removeEventListener('resize', displayBySize);
     });
 
+    useEffect(() => {
+        setIndex(0);
+    }, [data]);
+
     return (
         <React.Fragment>
-            <i className='fas fa-arrow-left prev-btn' onClick={() => slide(-1)}></i>
-            <i className='fas fa-arrow-right next-btn' onClick={() => slide(1)}></i>
-            <div className='projects'>
-            {dataToDisplay().map(project => (
+            <i className='fas fa-arrow-left slider-prev-btn' onClick={() => slide(-1)}></i>
+            <i className='fas fa-arrow-right slider-next-btn' onClick={() => slide(1)}></i>
+            <div className='slider-bullets'>
+            {data.map((project, i) => (
+                <div key={project.id} className={index === i && 'selected'} onClick={() => setIndex(i)}>
+                    <span>{project.title}</span>
+                </div>
+            ))}
+            </div>
+            <div className='slider-display'>
+            {dataToDisplay.map(project => (
                 <Project {...project}
                     key={project.id}
                     update={actions.update}
